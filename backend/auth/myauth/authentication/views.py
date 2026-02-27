02620a70bac7074b86ee69243182f18832b24ca3
0xSayf <dindinferda16@gmail.com>
2026-02-27 01:48:50 +0000

    SA

--- backend/auth/myauth/authentication/views.py
+++ backend/auth/myauth/authentication/views.py
@@ -1,53 +1,59 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
-from rest_framework_simplejwt.tokens import RefreshToken
 from .models import UserAuth
-import json
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.hashers import check_password
 from datetime import timedelta
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.conf import settings
+from django.contrib.auth import authenticate
+from django.middleware import csrf
+import json
 
 @csrf_exempt
 def login(request):
+
     if request.method != "POST":
-        return JsonResponse({"error": "Method not allowed"}, status=405)
+        return Response({"error": "Method not allowed"}, status=405)
 
     body = json.loads(request.body)
     username = body.get("username")
     password = body.get("password")
 
-    user = UserAuth.objects.filter(username=username).first()
+    user = authenticate(username=username, password=password)
 
     if user is None:
-        return JsonResponse({"error": "User not found"}, status=404)
+        return Response({"error": "Invalid credentials"}, status=401)
 
-    if not check_password(password, user.password):
-        return JsonResponse({"error": "Wrong password"}, status=401)
-
+    if not user.is_active:
+        return Response({"error": "Account inactive"}, status=403)
+    
     refresh = RefreshToken.for_user(user)
-    access_token = str(refresh.access_token)
+    access = refresh.access_token
 
-    response = JsonResponse({"message": "Login successful"}, status=200)
+    response = Response({"message": "Login successful"})
 
     response.set_cookie(
         key="access_token",
-        value=access_token,
+        value=str(access),
+        max_age=300,  
         httponly=True,
-        secure=False,     
-        samesite="Lax",     
-        max_age=3600        
+        secure=False, 
+        samesite="Lax"
     )
 
     response.set_cookie(
         key="refresh_token",
         value=str(refresh),
+        max_age=604800,  
         httponly=True,
-        secure=False, 
-        samesite="Lax",
-        max_age=7*24*3600 
+        secure=False,
+        samesite="Lax"
     )
+    csrf.get_token(request)
 
     return response
 
@@ -96,17 +102,3 @@ def logout(request):
     response.delete_cookie("access_token")
     response.delete_cookie("refresh_token")
     return response
-
-@csrf_exempt
-def protected_view(request):
-    access_token = request.COOKIES.get("access_token")
-    if not access_token:
-        return JsonResponse({"error": "Unauthorized"}, status=401)
-    jwt_auth = JWTAuthentication()
-    try:
-        validated_token = jwt_auth.get_validated_token(access_token)
-        user = jwt_auth.get_user(validated_token)
-    except Exception:
-        return JsonResponse({"error": "Invalid or expired token"}, status=401)
-
-    return JsonResponse({"message": f"Hello {user.username}, this is protected!"}, status=200)
